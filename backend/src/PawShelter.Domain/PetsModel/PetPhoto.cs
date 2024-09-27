4242bfcd08a702c0e1d908e5a2e65b430e7c2ba7
? <gusev-sasha-01@mail.ru>
2024-09-27 18:54:15 +0700

    some fixes

--- backend/src/PawShelter.Domain/PetsModel/PetPhoto.cs
+++ backend/src/PawShelter.Domain/PetsModel/PetPhoto.cs
@@ -3,6 +3,11 @@ namespace PawShelter.Domain.PetsModel
 {
     public record PetPhoto
     {
+        public PetPhoto(string path, bool isMain)
+        {
+            Path = path;
+            IsMain = isMain;
+        }
         public string Path { get; } = null!;
         public bool IsMain { get; }
     }
