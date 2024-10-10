faec9fe8088e9ec05a0db144ee2d49e06921f7e2
? <gusev-sasha-01@mail.ru>
2024-10-10 20:23:41 +0700

    add serilog, seq

---
+++ backend/src/PawShelter.API/Extensions/AppExtension.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PawShelter.Infrastructure;
+
+namespace PawShelter.API.Extensions;
+
+public static class AppExtension
+{
+    public static async Task ApplyMigrations(this WebApplication app)
+    {
+        await using var scope = app.Services.CreateAsyncScope();
+
+        var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+        await dbContext.Database.MigrateAsync();
+    }
+}
--- backend/src/PawShelter.API/Program.cs
+++ backend/src/PawShelter.API/Program.cs
@@ -1,9 +1,34 @@
 using FluentValidation;
 using Microsoft.EntityFrameworkCore;
+using PawShelter.API.Extensions;
 using PawShelter.Application;
 using PawShelter.Infrastructure;
+using Serilog;
+using Serilog.Events;
 
 var builder = WebApplication.CreateBuilder(args);
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.Debug()
+    .WriteTo.Seq(builder.Configuration.GetConnectionString("Seq")
+                 ?? throw new ArgumentNullException("Seq"))
+    .Enrich.WithThreadId()
+    .Enrich.WithThreadName()
+    .Enrich.WithEnvironmentName()
+    .Enrich.WithMachineName()
+    .Enrich.WithEnvironmentUserName()
+    .MinimumLevel.Override("Microsoft.AspNetCore.Hosting", LogEventLevel.Warning)
+    .MinimumLevel.Override("Microsoft.AspNetCore.Mvc", LogEventLevel.Warning)
+    .MinimumLevel.Override("Microsoft.AspNetCore.Routing", LogEventLevel.Warning)
+    .CreateLogger();
+
+builder.Services.AddHttpLogging(o =>
+{
+    o.CombineLogs = true;
+});
+
+builder.Services.AddSerilog();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -19,15 +44,14 @@ builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
 
 var app = builder.Build();
 
+app.UseSerilogRequestLogging();
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 
-    await using var scope = app.Services.CreateAsyncScope();
-    var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-
-    await dbContext.Database.MigrateAsync();
+    //await app.ApplyMigrations();
 }
 
 // Configure the HTTP request pipeline.
