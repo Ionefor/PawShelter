c783dbc9a6c40069519bd2b084f871af9f070e9e
? <gusev-sasha-01@mail.ru>
2024-11-10 21:47:41 +0700

    sorting && filtering query with pattern buider

--- backend/src/PawShelter.Application/Dto/QueryDto/SortingParamsDto.cs
+++ backend/src/PawShelter.Application/Dto/QueryDto/SortingParamsDto.cs
@@ -6,6 +6,7 @@ public record SortingParamsDto(
     bool? Color,
     bool? Species,
     bool? Breed,
-    bool? PetCharacteristics,
+    bool? Height,
+    bool? Weight,
     bool? BirthDate,
     bool? Address);
--- backend/src/PawShelter.Application/Extensions/QueriesExtensions.cs
+++ backend/src/PawShelter.Application/Extensions/QueriesExtensions.cs
@@ -31,6 +31,7 @@ public static class QueriesExtensions
         bool condition,
         Expression<Func<T, bool>> predicate)
     {
+        var a = predicate.Name;
         return condition ? source.Where(predicate) : source;
     }
     
@@ -42,4 +43,91 @@ public static class QueriesExtensions
         return condition ? source.OrderBy(selector) : source;
     }
 
+    public static IQueryable<TSource> ApplyFilter<TSource>(
+        this IQueryable<TSource> source,
+        object? filterParams)
+    {
+        if (filterParams is null)
+            return source;
+        
+        var type = filterParams.GetType();
+        
+        var properties = type.GetProperties(
+            BindingFlags.Public | BindingFlags.Instance);
+        
+        foreach (var property in properties)
+        {
+            var value = property.GetValue(filterParams);
+
+            if(value is null)
+                continue;
+            
+            if (!property.PropertyType.IsPrimitive &&
+                property.PropertyType != typeof(string) && 
+                property.PropertyType != typeof(Guid?))
+            {
+                source = source.ApplyFilter<TSource>(value);
+            }
+            else
+            {
+                var propertyName = property.Name;
+                var parameter = Expression.Parameter(typeof(TSource), "x");
+                var member = Expression.Property(parameter, propertyName);
+                var constant = Expression.Constant(value);
+                var equality = Expression.Equal(member, constant);
+                var lambda = Expression.Lambda<Func<TSource, bool>>(equality, parameter);
+
+                source = source.Where(lambda);
+            } 
+        }
+        
+        return source;
+    }
+    public static IQueryable<TSource> ApplySorting<TSource>(
+        this IQueryable<TSource> source,
+        object? sortingParams)
+    {
+        if (sortingParams is null)
+            return source;
+        
+        var type = sortingParams.GetType();
+        var properties = type.GetProperties(
+            BindingFlags.Public | BindingFlags.Instance);
+        
+        IOrderedQueryable<TSource>? orderedQuery = null;
+        
+        foreach (var property in properties)
+        {
+            var value = property.GetValue(sortingParams);
+
+            if(value is null)
+                continue;
+            
+            if((bool)value == false)
+                continue;
+            
+            var parameter = Expression.Parameter(typeof(TSource), "x");
+            var propertyAccess = Expression.Property(parameter, property.Name);
+            var orderByExp = Expression.Lambda(propertyAccess, parameter);
+            
+            PropertyInfo? propertyInfo = typeof(TSource).
+                GetProperty(property.Name);
+            
+            if(propertyInfo is null)
+                continue;
+            
+            Type propertyType = propertyInfo.PropertyType;
+            
+            var methodName = orderedQuery == null ? "OrderBy" : "ThenBy";
+            
+            var method = typeof(Queryable).GetMethods()
+                .First(m => m.Name == methodName && m.GetParameters().Length == 2).
+                    MakeGenericMethod(typeof(TSource), propertyType);
+            
+            orderedQuery = (IOrderedQueryable<TSource>)method.
+                Invoke(null, new object[] { orderedQuery ?? source, orderByExp })!;
+        }
+        
+        return orderedQuery ?? source;
+    }
 }
